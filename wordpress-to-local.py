829188bf1a7002c96222b1fd966fd3301934c057
Jeff <iobyhs@gmail.com>
2020-11-01 13:04:35 -0800

    Fixed change of domain to localhost in sql

--- wordpress-to-local.py
+++ wordpress-to-local.py
@@ -70,7 +70,7 @@ def main():
     # Replace with localhost in sql dump file
     with fileinput.FileInput(local_path + domain + '.sql', inplace=True, backup='.bak') as file:
         for line in file:
-            print(line.replace('(1,\'siteurl\',\'https://' + domain + '\',\'yes\')', '(1,\'siteurl\',\'http://' + domain + '\',\'yes\')').replace('(2,\'home\',\'https://' + domain + '\',\'yes\')', '(2,\'home\',\'http://localhost\',\'yes\')'), end='')
+            print(line.replace('(1,\'siteurl\',\'https://' + domain + '\',\'yes\')', '(1,\'siteurl\',\'http://localhost:8000\',\'yes\')').replace('(2,\'home\',\'https://' + domain + '\',\'yes\')', '(2,\'home\',\'http://localhost:8000\',\'yes\')'), end='')
 
 
 if __name__ == '__main__':
