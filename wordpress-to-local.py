dc06af3037f1890e29d11a3954ac11ad268b1132
Jeff <iobyhs@gmail.com>
2020-11-01 00:02:34 -0700

    Added __pycache__/ to .gitignore

--- wordpress-to-local.py
+++ wordpress-to-local.py
@@ -12,6 +12,8 @@ from config import (
     local_path,
     remote_path,
     db_name,
+    db_username,
+    db_password,
     filename,
     domain
 )
@@ -29,7 +31,7 @@ def createSSHClient(server, user, sshkey):
 
 
 ssh = createSSHClient(host, user, ssh_key)
-commands = ['tar --exclude=\'' + domain + '/wp-content/uploads/\' -czf \'' + domain + '/' + filename + '.tar.gz\' ' + domain, 'mysqldump --add-drop-table ' + db_name + ' | gzip > ' + remote_path + filename + '.sql.gz']
+commands = ['tar --exclude=\'' + domain + '/wp-content/uploads/\' -czf \'' + domain + '/' + filename + '.tar.gz\' ' + domain, 'mysqldump -u ' + db_username + ' -p' + db_password + ' --add-drop-table ' + db_name + ' | gzip > ' + remote_path + filename + '.sql.gz']
 
 def main():
     for command in commands:
@@ -61,12 +63,12 @@ def main():
 
     # Extract sql dump file
     with gzip.open(local_path + filename + '.sql.gz', 'rb') as f_in:
-        with open(local_path + filename + '.sql', 'wb') as f_out:
+        with open(local_path + domain + '.sql', 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
 
     # Replace with localhost in sql dump file
-    with fileinput.FileInput(local_path + filename + '.sql', inplace=True, backup='.bak') as file:
+    with fileinput.FileInput(local_path + domain + '.sql', inplace=True, backup='.bak') as file:
         for line in file:
             print(line.replace('(1,\'siteurl\',\'https://' + domain + '\',\'yes\')', '(1,\'siteurl\',\'http://' + domain + '\',\'yes\')').replace('(2,\'home\',\'https://' + domain + '\',\'yes\')', '(2,\'home\',\'http://localhost\',\'yes\')'), end='')
 
